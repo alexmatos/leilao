9c2c01f7a1926a56fbf3a7fd45e250d364a49767
Alex Matos da Silva <alexmatos.br@gmail.com>
2017-04-03 10:58:10 -0400

    Início da aula 3

--- Leilao.php
+++ Leilao.php
@@ -1,23 +1,47 @@
 <?php
-	class Leilao {
-		private $descricao;
-		private $lances;
-		
-		function __construct($descricao) {
-			$this->descricao = $descricao;
-			$this->lances = array();
-		}
-		
-		public function propoe(Lance $lance) {
-			$this->lances[] = $lance;
-		}
 
-		public function getDescricao() {
-			return $this->descricao;
-		}
+class Leilao {
 
-		public function getLances() {
-			return $this->lances;
-		}
-	}
+    private $descricao;
+    private $lances;
+
+    function __construct($descricao) {
+        $this->descricao = $descricao;
+        $this->lances = array();
+    }
+
+    public function propoe(Lance $lance) {
+        if (count($this->lances) == 0 || $this->podeDarLance($lance->getUsuario())) {
+            $this->lances[] = $lance;
+        }
+    }
+
+    private function podeDarLance(Usuario $usuario) {
+        return !$this->ultimoLanceDado()->getUsuario()->getNome() == $usuario->getNome() 
+                && $this->qtdDelancesDo($usuario) < 5;
+    }
+
+    private function qtdDelancesDo(Usuario $usuario) {
+        $total = 0;
+        foreach ($lances as $lance) {
+            if ($lance->getUsuario()->getNome() == $usuario->getNome())
+                $total++;
+        }
+        return $total;
+    }
+
+    private function ultimoLanceDado() {
+        return $this->lances[count($this->lances) - 1];
+    }
+
+    public function getDescricao() {
+        return $this->descricao;
+    }
+
+    public function getLances() {
+        return $this->lances;
+    }
+
+}
+
 ?>
